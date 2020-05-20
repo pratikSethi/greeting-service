7e9d90cdfdb808a5d6e776ed012997e0ba9ca847
Pratik Sethi <pratik.root@gmail.com>
2020-05-19 23:17:01 -0700

    Added root context path for our API

--- src/main/java/example/topic/TopicController.java
+++ src/main/java/example/topic/TopicController.java
@@ -12,27 +12,27 @@ public class TopicController {
     @Autowired
     private TopicService topicService;
 
-    @RequestMapping("/topics")
+    @GetMapping("/topics")
     public List<Topic> getAllTopics(){
         return topicService.getAllTopics();
     }
 
-    @RequestMapping("/topics/{id}")
+    @GetMapping("/topics/{id}")
     public Topic getTopic(@PathVariable int id){
         return topicService.getTopic(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/topics")
+    @PostMapping("/topics")
     public void addTopic(@RequestBody Topic topic){
         topicService.addTopic(topic);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/topics/{id}")
+    @PutMapping("/topics/{id}")
     public void updateTopic(@RequestBody Topic topic, @PathVariable int id){
         topicService.updateTopic(id, topic);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/topics/{id}")
+    @DeleteMapping("/topics/{id}")
     public void deleteTopic(@PathVariable int id){
         topicService.deleteTopic(id);
     }
